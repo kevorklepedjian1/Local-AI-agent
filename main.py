f9782c21ed5044680577e4faadb2336b6427d0c8
Kevork Lepedjian <99024590+kevorklepedjian1@users.noreply.github.com>
2026-03-17 13:10:35 +0200

    Improve question handling and review retrieval
    Refactor input handling and integrate retriever for reviews.

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
-
-# how to utilize ollama model
 model = OllamaLLM(model="llama3.2")
 
 template = """
-You are an expert in answering questions about a pizza restaurant
+You are an exeprt in answering questions about a pizza restaurant
 
-Here are some relevant review: {reviews}
+Here are some relevant reviews: {reviews}
 
 Here is the question to answer: {question}
 """
-
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-while True :
- print("------------")
- question = input("Ask any question")
- print("\n\n")
- if question == "q":
-  break
-
- result =chain.invoke({"reviews" : [] , "question":"what is the best pizza in town"})
-
- print(result)
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
